f3b1de118c94a9e33e2b445f535a380ad5e68b50
Chaoran Wang <showcy@vt.edu>
2022-03-09 17:40:55 -0800

    Add filters and timeline file. Remove the python dependencies.

---
+++ init.R
@@ -0,0 +1,14 @@
+# R script to run author supplied code, typically used to install additional R packages
+# contains placeholders which are inserted by the compile script
+# NOTE: this script is executed in the chroot context; check paths!
+
+r <- getOption('repos')
+r['CRAN'] <- 'http://cloud.r-project.org'
+options(repos=r)
+
+# ======================================================================
+
+# packages go here
+install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes', 'dashCoreComponents', 'dashHtmlComponents', 'dashBootstrapComponents', 'ggplot2', 'tidyverse',
+                   'GGally', 'plotly'))
+remotes::install_github('facultyai/dash-bootstrap-components@r-release')
